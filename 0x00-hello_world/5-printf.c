fe8222ad664c78f224ff98c4a95bbe398153c7da
Buchii <chizobaOG@AOL.com>
2021-06-17 12:29:06 -0700

    5-printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *Return: retun 1 after program execution
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
