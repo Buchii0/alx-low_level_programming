d997c9c2d05fb809cc7a8bf2a6e47dde5e103317
Okonkwo Godspower Onyebuchi <okonkwogodspower@yahoo.com>
2021-06-18 21:28:15 +0100

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,17 +17,17 @@ n = rand() - RAND_MAX / 2;
 
 if (n > 5)
 {
-printf("Last digit of ""%d is %d and is greater tham 5\n", n);
+printf("Last digit of "%d is %d and is greater than 5\n", n);
 }
 
 else if (n == 0)
 {
-printf("Last digit of ""%d is %d and is 0\n", n);
+printf("Last digit of "%d is %d and is 0\n", n);
 }
 
 else if (n < 6 && != 0)
 {
-printf("Last digit of ""%d is %d and is less than 6 and not 0\n", n);
+printf("Last digit of "%d is %d and is less than 6 and not 0\n", n);
 }
 
 return (0);
