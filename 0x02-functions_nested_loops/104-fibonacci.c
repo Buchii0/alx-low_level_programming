5254dbc10747dafa4f0747306c66e55851566163
Okonkwo Godspower Onyebuchi <okonkwogodspower@yahoo.com>
2021-06-22 20:31:27 +0100

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,47 +1,55 @@
 #include <stdio.h>
-#define Z 1000000000
 /**
- * main - increasing and decreasing until 98
- *
- * Return: Always 0.
+ *numLength - returns the length of string
+ *@num: operand number
+ *Return: number of digits
+ */
+int numLength(int num)
+{
+int length = 0;
+
+if (!num)
+{
+return (1);
+}
+while (num)
+{
+num = num / 10;
+length += 1;
+}
+return (length);
+}
+/**
+ * main - prints the first 98 digits of the fibonacci series
+ * Return: 0
  */
 int main(void)
 {
-unsigned long int a, a1, a2, b, b1, b2, r, r1, r2, i;
+unsigned long fib1 = 1, fib2 = 2, next, mx = 100000000, fib1o = 0, fib2o = 0, nexto = 0;
+short int i = 1, initial0s;
 
-a = 1;
-b = 2;
-r = a + b;
-
-printf("%lu, %lu,", a, b);
-
-for (i = 0; i < 86; i++)
+while (i <= 98)
 {
-printf("%lu", r);
-a = b;
-b = r;
-r = a + b;
-}
-
-b1 = b / Z;
-b2 = b % Z;
-r1 = r / Z;
-r2 = r % Z;
-
-for (i = 0; i < 9; i++)
+if (fib1o > 0)
+printf("%lu", fib1o);
+initial0s = numLength(mx) - 1 - numLength(fib1);
+while (fib1o > 0 && initial0s > 0)
 {
-printf("%lu%lu,", r1, r2);
-a1 = b1;
-a2 = b2;
-b1 = r1;
-b2 = r2;
-
-r1 = a1 + b1 + (a2 + b2) / Z;
-r2 = (a2 + b2) % Z;
+printf("%i", 0);
+initial0s--;
 }
-
-printf("%lu%lu\n", r1, r2);
-
+printf("%lu", fib1);
+next = (fib1 + fib2) % mx;
+nexto = fib1o + fib2o + (fib1 + fib2) / mx;
+fib1 = fib2;
+fib1o = fib2o;
+fib2 = next;
+fib2o = nexto;
+if (i != 98)
+printf(", ");
+else
+printf("\n");
+i++;
+}
 return (0);
-
 }
