ba1fdf7dc73401cf508b84d15c2fe0304ccab79e
Okonkwo Godspower Onyebuchi <okonkwogodspower@yahoo.com>
2021-06-28 19:57:27 +0100

    Create 101-keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ */
+int main(void)
+{
+int num;
+
+srand(time(0));
+num = rand();
+printf("%i\n", num);
+return (0);
+}
