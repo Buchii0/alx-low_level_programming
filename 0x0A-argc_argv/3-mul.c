2a10e716f5caa1f77377219575f8d770b60ba57a
Okonkwo Godspower Onyebuchi <okonkwogodspower@yahoo.com>
2021-07-12 20:35:15 +0100

    Update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,45 +1,24 @@
+#include "holberton.h"
+#include <stdlib.h>
 #include <stdio.h>
-#include<stdlib.h>
 
 /**
- * main - Print the name of the program
- * @argc: Count arguments
- * @argv: Arguments
- *
- * Return: Always 0 (Success)
+ * main - prints the multiplication of two integers
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 if true, 1 if false
  */
-
 int main(int argc, char *argv[])
-
 {
-	/*Declaring variables*/
-	int count;
-	int first_num;
-	int second_num;
-	int result;
+int a, b;
 
-	(void) argc; /*Ignore argc*/
-
-	count = 1;
-	if (argv[count] != '\0') /*Goes through the whole array*/
-	{
-		/*Condition If the program does not receive two arguments*/
-		if (argv[count] == NULL || argv[count + 1] == NULL)
-		{
-			printf("Error\n");
-			return (1);
-		}
-
-		else /*Condition If the program does receive two arguments*/
-		{
-			first_num = atoi(argv[count]); /*ATOI --> convert string to int*/
-			second_num = atoi(argv[count + 1]);
-			result = first_num * second_num;
-
-			printf("%d\n", result);
-		}
-
-	}
-
-	return (0);
+if (argc == 3)
+{
+a = atoi(argv[1]);
+b = atoi(argv[2]);
+printf("%d\n", a *b);
+return (0);
+}
+printf("Error\n");
+return (1);
 }
