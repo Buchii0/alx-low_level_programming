d61c5edac1f08a878add821913621f6a102817a8
Okonkwo Godspower Onyebuchi <okonkwogodspower@yahoo.com>
2021-07-19 21:38:10 +0100

    Update 2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-#include "holberton.h"
-
 /**
- *main - print name file
- *Return: Always 0
+ * main - prints the name of the file program was compiled from
+ * Return: Always 0
  */
 
 int main(void)
 {
-	printf("%s\n", __FILE__);
-	return (0);
+printf("%s\n", __FILE__);
+return (0);
 }
